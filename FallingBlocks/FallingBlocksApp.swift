86127ce89be2d91902aeb6c9a2ba9168ba41416b
Jonathan Nobels <jnobels@gmail.com>
2023-07-02 22:05:22 -0400

    Initial Commit

---
+++ FallingBlocks/FallingBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallingBlocksApp.swift
+//  FallingBlocks
+//
+//  Created by Jonathan Nobels on 2023-07-02.
+//
+
+import SwiftUI
+
+@main
+struct FallingBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
